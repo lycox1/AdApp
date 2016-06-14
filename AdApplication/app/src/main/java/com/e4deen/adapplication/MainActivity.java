10dcd077f90095969091057d0ae2fde48a519e5b
lycox1 <lycox1@gmail.com>
2016-06-15 00:34:32 +0900

    2016-06-14 Navigation Drawer is working

--- AdApplication/app/src/main/java/com/e4deen/adapplication/MainActivity.java
+++ AdApplication/app/src/main/java/com/e4deen/adapplication/MainActivity.java
@@ -1,15 +1,27 @@
 package com.e4deen.adapplication;
 
+import android.content.res.Configuration;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
+//public class MainActivity extends AppCompatActivity {
+public class MainActivity extends ActionBarActivity {
+
+    final String LOG_TAG = "e4deen_MainActivity";
+    ActionBarDrawerToggle drawerToggle;
+    String [] drawer_str={"mypage","setup","help"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,14 +40,14 @@ public class MainActivity extends AppCompatActivity {
         listview = (ListView) findViewById(R.id.listview1);
         listview.setAdapter(adapter);
 
-        // 첫 번째 아이템 추가.
+        // ? ?? ??? ??.
         //adapter.addItem(ContextCompat.getDrawable(this, R.drawable.ic_account_box_black_36dp),
         adapter.addItem(ContextCompat.getDrawable(this, R.drawable.pic1),
                 "Box", "Account Box Black 36dp") ;
-        // 두 번째 아이템 추가.
+        // ? ?? ??? ??.
         adapter.addItem(ContextCompat.getDrawable(this, R.drawable.pic2),
                 "Circle", "Account Circle Black 36dp");
-        // 세 번째 아이템 추가.
+        // ? ?? ??? ??.
         adapter.addItem(ContextCompat.getDrawable(this, R.drawable.pic3),
                 "Ind", "Assignment Ind Black 36dp");
 
@@ -63,6 +75,52 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 */
+
+//-------------------------------------------------------------------------------------
+        DrawerLayout drawerLayout=(DrawerLayout)findViewById(R.id.drawerlayout);
+        ListView listView=(ListView)findViewById(R.id.drawer);
+        ArrayAdapter<String> adapterNav=new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,drawer_str);
+        listView.setAdapter(adapterNav);
+        drawerToggle=new ActionBarDrawerToggle(this,drawerLayout,R.string.open,R.string.close){
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+            }
+        };
+        drawerLayout.setDrawerListener(drawerToggle);
+        float elevationNav = drawerLayout.getDrawerElevation();
+        Log.d(LOG_TAG,"elevationNav " + elevationNav);
+        drawerLayout.setDrawerElevation(elevationNav + 10);
+
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+
+        //setDrawerElevation(float elevation)
+
     }
 
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        drawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        drawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(drawerToggle.onOptionsItemSelected(item))
+            return true;
+        return super.onOptionsItemSelected(item);
+    }
 }
